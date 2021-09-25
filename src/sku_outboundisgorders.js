0e9e8ebaa41e91fd4c835bd145648cadb77cc22e
Daniel Chua <ask@interstellargoods.sg>
2021-09-25 10:08:04 +0800

    tester commit

--- src/sku_outboundisgorders.js
+++ src/sku_outboundisgorders.js
@@ -1,81 +1,79 @@
 "use strict";
+//new remote
+const {
+  BESKUInboundService,
+  BESKUOutboundISGOrderService,
+  BackendlessService,
+} = require("./services/backendless");
 
 const {
-    BESKUInboundService,
-    BESKUOutboundISGOrderService,
-    BackendlessService,
-} = require('./services/backendless');
-
-const { 
-    SKUOutboundISGOrderService,
-} = require('./services/sku_outboundisgorders');
+  SKUOutboundISGOrderService,
+} = require("./services/sku_outboundisgorders");
 
 const skuOutboundISGOrderService = new SKUOutboundISGOrderService(
-    new BESKUInboundService(),
-    new BESKUOutboundISGOrderService(),
-    new BackendlessService(),
-)
+  new BESKUInboundService(),
+  new BESKUOutboundISGOrderService(),
+  new BackendlessService()
+);
 
-const sendOut = async(event) => {
-    try {
-        console.log({
-            message: "Incoming request",
-            data: JSON.parse(event.body),
-        });
-        
+const sendOut = async (event) => {
+  try {
+    console.log({
+      message: "Incoming request",
+      data: JSON.parse(event.body),
+    });
 
-        console.log(event.pathParameters.sku_outbound_isgorder_id)
+    console.log(event.pathParameters.sku_outbound_isgorder_id);
 
+    const body = JSON.parse(event.body);
 
-        const body = JSON.parse(event.body);
+    await skuOutboundISGOrderService.sendOut(
+      event.pathParameters.sku_outbound_isgorder_id,
+      body.status,
+      body.warehouse
+    );
 
-        await skuOutboundISGOrderService.sendOut(
-            event.pathParameters.sku_outbound_isgorder_id,
-            body.status,
-            body.warehouse,
-        )
-        
-        let res = {
-            statusCode: 200,
-            body: JSON.stringify(body),
-          };
-      
-          console.log({
-            message: "Outgoing response",
-            data: res,
-          });
+    let res = {
+      statusCode: 200,
+      body: JSON.stringify(body),
+    };
 
-        return res;
-    } catch(err) {
-        console.log(err);
+    console.log({
+      message: "Outgoing response",
+      data: res,
+    });
 
-        if(err.message == "sku_inbound_notfound") {
-            return {
-                statusCode: 400,
-                body: JSON.stringify({
-                    message: "No SKU_Inbound that links to this ID found",
-                }),
-            };
-        }
+    return res;
+  } catch (err) {
+    console.log(err);
 
-        if(err.message == "Insufficient") {
-            return {
-                statusCode: 400,
-                body: JSON.stringify({
-                    message: "Insufficient items in SKU_Inbound",
-                }),
-            };
-        }
-
-        return {
-            statusCode: 500,
-            body: JSON.stringify({
-                message: "INTERNAL_SERVER_ERROR",
-            }),
-        };
+    if (err.message == "sku_inbound_notfound") {
+      return {
+        statusCode: 400,
+        body: JSON.stringify({
+          message: "No SKU_Inbound that links to this ID found",
+        }),
+      };
     }
-}
+
+    if (err.message == "Insufficient") {
+      return {
+        statusCode: 400,
+        body: JSON.stringify({
+          message: "Insufficient items in SKU_Inbound",
+        }),
+      };
+    }
+
+    return {
+      statusCode: 500,
+      body: JSON.stringify({
+        message: "INTERNAL_SERVER_ERROR",
+      }),
+    };
+  }
+};
 
 module.exports = {
-    sendOut,
-}
+  sendOut,
+};
